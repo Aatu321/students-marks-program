c60f74b1aaf44550e3c0ce69b1a086b236638ae5
yashmini_21 <miholiayashmini@gmail.com>
2025-03-26 03:41:49 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,10 +1,11 @@
 marks = int(input("enter your student marks : "))
 
- if(marks >= 90):
+if(marks >= 90):
      grade = "A"
- elif(marks >= 80 and marks < 90):
+elif(marks >= 80 and marks < 90):
      grade = "B"
- elif(marks >= 70 and marks < 80):
+elif(marks >= 70 and marks < 80):
      grade = "C"
- else:
+else:
      grade = "D"
+print("grade of the student ->", grade)     
